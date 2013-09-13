a04f8575f5c8f7e59aa4aa13236119f8cbfbebf6
akorchan <gprogramador@gmail.com>
2013-09-13 23:01:09 +0300

    add modal dialog

--- public/js/controllers/admin-controller.js
+++ public/js/controllers/admin-controller.js
@@ -2,8 +2,13 @@
 
 /** Controllers */
 angular.module('store.controllers')
-    .controller('AdminController', function ($scope, storeItems, $http, $timeout, $location) {
+    .controller('AdminController', function ($scope, storeItems, $http, $timeout, $dialog, $location) {
         storeItems.getAllItems(function (data) {
             $scope.items = data;
         });
+        $scope.addItemDialog = function () {
+//            $timeout(function(){
+            $dialog.dialog({}).open('public/partials/dialogs/add-item-dialog.html');
+//            }, 3000);
+        }
     });
