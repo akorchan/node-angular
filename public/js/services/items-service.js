abe99e20c1554813d8432ce3a5af2def5d6e5751
akorchan <gprogramador@gmail.com>
2013-09-16 22:53:59 +0300

    saving file, but still with wrong name

--- public/js/services/items-service.js
+++ public/js/services/items-service.js
@@ -35,6 +35,41 @@ angular.module('store.services').service('storeItems', function ($http) {
      * @param callback
      */
     var addItem = function (objectToStore, fileToStore, callback) {
+        $http({
+            method: 'POST',
+            url: "/items",
+            //IMPORTANT!!! You might think this should be set to 'multipart/form-data'
+            // but this is not true because when we are sending up files the request
+            // needs to include a 'boundary' parameter which identifies the boundary
+            // name between parts in this multi-part request and setting the Content-type
+            // manually will not set this boundary parameter. For whatever reason,
+            // setting the Content-type to 'false' will force the request to automatically
+            // populate the headers properly including the boundary parameter.
+            headers: { 'Content-Type': false },
+            //This method will allow us to change how the data is sent up to the server
+            // for which we'll need to encapsulate the model data in 'FormData'
+            transformRequest: function (data) {
+                var formData = new FormData();
+                //need to convert our json object to a string version of json otherwise
+                // the browser will do a 'toString()' on the object which will result
+                // in the value '[Object object]' on the server.
+                formData.append("object", angular.toJson(data.object));
+                //now add all of the assigned files
+                formData.append("file", data.file);
+                return formData;
+            },
+            //Create an object that contains the model and files which will be transformed
+            // in the above transformRequest method
+            data: { object: objectToStore, file: fileToStore }
+        }).
+            success(function (data, status, headers, config) {
+                callback();
+            }).
+            error(function (data, status, headers, config) {
+                console.log(data);
+            });
+
+
         $http({method: "POST", data: {object: objectToStore, file: fileToStore}, url: "/items" }).
             success(function (data) {
                 callback(data);
